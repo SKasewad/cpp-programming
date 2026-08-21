a8220a0f91c52907ac0e414a61d5df6f3d9bcc8f
Santoba kasewad <skasewad1908@gmail.com>
2026-08-21 12:53:04 +0530

    Refactor time addition to fraction addition

--- 28 time addition .cpp
+++ 28 time addition .cpp
@@ -1,38 +1,49 @@
 #include<iostream>
 using namespace std;
-class Time
+
+class fraction
 {
-   int hours,minutes,seconds;
+    int num,den;
+
 public:
-   Time(int h=0,int m=0,int s=0)
+   void input()
    {
-      hours=h;
-      minutes=m;
-      seconds=s;
-   }
-   Time add(Time t)
-   {
-      int s=seconds+t.seconds;
-      int m=minutes+t.minutes+s/60;
-      int h=hours+t.hours+m/60;
-      s=s%60;
-      m=m%60;
-      return Time(h,m,s);
-   }
-   void display()
-   {
-      cout<<hours<<":"<<minutes<<":"<<seconds<<endl;
-   }
+        cout<<"Enter numerator and denominator:";
+        cin>>num>>den;
+    }
+    fraction add(fraction f)
+    {
+        fraction temp;
+        temp.num=(num*f.den)+(f.num*den);
+        temp.den=den*f.den;
+        return temp;    
+    }
+    fraction sub(fraction f)
+    {
+      fraction temp;
+      temp.num=(num*f.den)-(f.num*den);
+      temp.den=den*f.den;
+      return temp;    
+    } 
+    void display()
+    {
+        cout<<num<<"/"<<den;
+    } 
 };
 int main()
 {
-   Time t1(10,45,30),t2(5,20,45);
-   Time result=t1.add(t2);
-   cout<<"First Time:";
-   t1.display();
-   cout<<"Second Time:";
-   t2.display();
-   cout<<"Addition:";
-   result.display();
-   return 0;
+    fraction f1,f2,f3;
+
+    f1.input();
+    f2.input();
+
+    f3=f1.add(f2);
+    cout<<"Addition=";
+    f3.display();
+
+    f3=f1.sub(f2);
+    cout<<"\nSubstraction=";
+    f3.display();
+
+    return 0;
 }
